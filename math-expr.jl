8d9a7adde0de208083e98009e11f6cbbcc3a3e29
Uri <56364336+uriballo@users.noreply.github.com>
2022-10-17 16:55:19 +0200

    feat: print tree + addNode

--- math-expr.jl
+++ math-expr.jl
@@ -22,9 +22,66 @@ Base.@kwdef mutable struct MathExpr
     rightChild :: Union{MathExpr, Nothing} = nothing
 end
 
-# TESTS
-randomExpr = MathExpr(constant= true, value = 2.0)
+function printTree(expr :: Union{MathExpr, Nothing})
+    if typeof(expr) == Nothing
+        return
+    end
 
-println(randomExpr.value)
+    printTree(expr.leftChild)
 
+    expr.constant ? print(expr.value) : (expr.operator ? print(expr.operatorId) : print("x$(expr.parameterId)"))
+
+    printTree(expr.rightChild)
+end
+
+# Given a math expr it adds a node to it.
+#   * note that it only adds to left/right child of the node passed as a param.
+function addNode(expr :: MathExpr, newNode :: MathExpr, position:: Int)
+    if position == 0
+        expr.leftChild = newNode
+    else 
+        expr.rightChild = newNode
+    end
+end
+
+function binaryOperationNode(op:: MathExpr, leftParam :: MathExpr, rightParam :: MathExpr)
+    addNode(op, leftParam, 0)
+    addNode(op, rightParam, 1)
+end
+
+function unaryOperationNode(op:: MathExpr, param :: MathExpr)
+    addNode(op, param, 0)
+end
+
+# Tests
+# 2.3 + 2.6
+expression = MathExpr(operator = true, operatorId = "+")
+
+addNode(expression, MathExpr(constant = true, value = 2.3), 0)
+addNode(expression, MathExpr(constant = true, value = 2.6), 1)
+
+printTree(expression)
+# x1^3 + 2x3 -1 
+# TODO: Fix Method ERROR
+plus = MathExpr(operator = true, operatorId = "+")
+minus = MathExpr(operator = true, operatorId = "-")
+cube = MathExpr(operator = true, operatorId = "³") 
+mult = MathExpr(operator = true, operatorId = "⋅")
+
+x1 = MathExpr(parameter = true, parameterId = 1)
+x3 = MathExpr(parameter = true, parameterId = 3)
+
+one = MathExpr(constant = true, value = 1.0)
+
+two = MathExpr(constant = true, value = 2.0)
+
+x1Cube = unaryOperationNode(cube, x1)
+twoX3 = binaryOperationNode(mult, x3, two)
+twoX3m1 = binaryOperationNode(minus, twoX3, one)
+
+expression2 = MathExpr(plus, x1Cube, twoX3m1)
+
+printTree(expression2)
+
+println()
 end
