2c799864b83115bba5ab476c6b61c140579629a1
Uri <56364336+uriballo@users.noreply.github.com>
2022-10-19 18:29:53 +0200

    feat: string to operator function

--- operators.jl
+++ operators.jl
@@ -1,18 +1,35 @@
 module operators
+using Match
 # New operators
 
-# Powers
+# Custom Operators
 x⁻¹(x) = 1/x
 x²(x)  = x^2
 x³(x)  = x^3
 x⁴(x)  = x^4
 x⁵(x)  = x^5
 
-# Exponentials
 eˣ(x) = ℯ^x # \euler to represent number 'e', Base.MathConstants.e
 
+√x(x) = x < 0: 0 ? sqrt(x)
+
 function strToOperator(operator:: String)::Function
-    #
+    @match operator
+        "^-1" => x⁻¹
+        "^2"  => x²
+        "^3"  => x³
+        "^4"  => x⁴
+        "^5"  => x⁵
+        "sin" => sin
+        "cos" => cos
+        "+"   => +
+        "*"   => *
+        "/"   => /
+        "log" => log
+        "e^"  => eˣ
+        "sqr" => √x
+        _     => identity
+        
 end
 
 end
